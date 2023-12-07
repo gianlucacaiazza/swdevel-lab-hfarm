0d4eaf915f122c69fd2deaad3aa16b6a0a29309d
gblasut <889942@stud.unive.it>
2023-12-07 19:59:27 +0100

    Developed corrZipTrees
    I've developed a function that return the zip code which respect the green area criteria given a user's input

--- backend/app/mymodules/data_handling.py
+++ backend/app/mymodules/data_handling.py
@@ -8,8 +8,20 @@ trees = pd.read_csv('Datasets/Trees.csv')
 zip = pd.read_csv('Datasets/zip_neighbourhood.csv')
 bnb = pd.read_csv('Datasets/AirBnb.csv')
 
+
 # Finds zipcodes that respects the criteria for the number of attractions
 def corrZipAtt(min, max):
     counts_by_zipcode = attractions.groupby('Zipcode')['Tourist_Spot'].count().reset_index()
     filtered_locations = counts_by_zipcode[(counts_by_zipcode['Tourist_Spot'] >= min) & (counts_by_zipcode['Tourist_Spot'] <= max)]
     return filtered_locations['Zipcode'].tolist()
+
+
+# Finds zipcodes that respects the criteria for green areas
+def corrZipTrees(trees_bool):
+    if(trees_bool == 'True'):
+        trees_mean = int(trees['count'].mean())
+        zipcodes_trees = trees[trees['count'] >= trees_mean]['zipcode'].tolist()
+    else:
+        zipcodes_trees = trees['zipcode'].tolist()
+    
+    return zipcodes_trees
