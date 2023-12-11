c81118c91cfc2dc704b0a1607c83968d843c4a01
LorenzoRizzo <888859@stud.unive.it>
2023-12-11 15:27:43 +0100

    Function implementation linked to Issue #4

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,7 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from datetime import datetime
 import pandas as pd
+import csv
 
 
 app = FastAPI()
@@ -26,6 +27,31 @@ def read_root():
         dict: A simple greeting.
     """
     return {"Hello": "World"}
+
+
+@app.get('/module/search/{provider_name}')
+def get_charging_points_by_provider(provider_name):
+    """
+    Retrieve charging points based on the provided provider's name.
+
+    Args:
+    - provider_name (str): The name of the charging point provider.
+
+    Returns:
+    - List[dict]: List of dictionaries containing charging point information.
+    """
+    charging_points = []
+    with open('/app/app/ricarica_colonnine.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        
+        for row in reader:
+            if row['titolare'].upper() == provider_name.upper():
+                charging_points.append({
+                    'localita': row['localita'].capitalize(),
+                    'tipologia': row['tipologia'],
+                    'numero_col': row['numero_col']
+                })
+    return charging_points
 
 
 @app.get('/get-date')
