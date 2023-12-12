b2ba4882c1933260a0f27ee211a42fcbef0f9ead
martaperissinotto <150264067+martaperissinotto@users.noreply.github.com>
2023-12-12 14:07:43 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 app = FastAPI()
 
-df = pd.read_csv('/app/app/ricarica_colonnine.csv')
+df = pd.read_csv('/app/app/ricarica_colonnine.csv',sep=';')
 
 
 @app.get('/')
@@ -26,6 +26,45 @@ def read_root():
         dict: A simple greeting.
     """
     return {"Hello": "World"}
+
+
+@app.get('/get_charging_stations')
+def numbers_of_stations_per_via(street_name):
+    selected_street = df[df['nome_via'] == street_name]
+    
+    if not selected_street.empty:
+        number_of_stations = selected_street['numero_col'].sum()  # Sum the column values for the selected street
+        info_vie = number_of_stations
+        return f"Number of charging stations in '{street_name}': {info_vie}"
+    else:
+        return f"The sreet '{street_name}' is not present in the dataset."
+
+@app.get('/get_charging_stations/{street_name}')
+def numbers_of_stations_per_via(street_name: str):
+    """
+    Endpoint to get the number of charging columns based on the street name.
+
+    Args:
+        nome_via (str): The name of the street.
+
+    Returns:
+        dict: Information about the number of charging columns for the provided street name.
+    """
+    street_name = street_name.upper()  # Convert to title case for consistency
+    selected_street = df[df['nome_via'] == street_name]
+    
+    if not selected_street.empty:
+        number_of_stations = selected_street['numero_col'].sum()  # Sum the column values for the selected street
+        return {"street_name": street_name, "number_stations": str(number_of_stations)}
+    else:
+        return {"error": f"The street '{street_name}' is not present in the dataset."}
+
+def numbers_of_stations_per_via(street_name_input: str = None):
+    if street_name_input is not None:
+        result = numbers_of_stations_per_via(street_name_input)
+        return result
+    else:
+        return '/'
 
 
 @app.get('/get-date')
