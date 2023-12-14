864b0913f1bd0a78708f22a2f3c0c5c66ae111c8
MatiLoca <matilde.farinelli@student.h-farm.com>
2023-12-14 16:45:14 +0100

    Added dockstring in info.py

--- backend/tests/test_info.py
+++ backend/tests/test_info.py
@@ -16,6 +16,8 @@ spotify_songs = pd.read_csv('/app/app/spotify_songs.csv')
 
 
 def test_genre_popularity():
+    """Test the genre_popularity function to ensure it returns a dictionary with genres
+    as keys and floats as values, with popularity scores between 0 and 100."""
     result = genre_popularity()
     assert isinstance(result, dict)
     for genre, popularity in result.items():
@@ -24,18 +26,25 @@ def test_genre_popularity():
 
 
 def test_get_song_count_by_genre():
+  """Test the get_song_count_by_genre function to ensure it returns a dictionary with
+    subgenres as keys and integers as values, with the sum of values matching the
+    total number of songs in the dataset."""  
     result = get_song_count_by_genre()
     assert isinstance(result, dict)
     assert sum(result.values()) == len(spotify_songs)
 
 
 def test_count_songs():
+    """Test the count_songs function to ensure it returns an integer count of all
+    songs in the dataset."""
     result = count_songs('path_to_your_csv')
     assert isinstance(result, int)
     assert result == len(spotify_songs)
 
 
 def test_artist_songs():
+    """Test the artist_songs function to ensure it returns a dictionary with artists
+    as keys and integers as values, sorted by artist name."""
     result = artist_songs()
     assert isinstance(result, dict)
     assert list(result.keys()) == sorted(result.keys())
