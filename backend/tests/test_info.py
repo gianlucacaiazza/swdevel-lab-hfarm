43b4bea479eb68644ff8e8a510aa8be1132d434b
U20-LSPD <umberto.bolzoni@student.h-farm.com>
2023-12-15 16:30:12 +0100

    make code PEP-8 compliance

--- backend/tests/test_info.py
+++ backend/tests/test_info.py
@@ -2,22 +2,25 @@ import os
 import sys
 import pandas as pd
 from fastapi.testclient import TestClient
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app.main import app
+from app.mymodules.info import (genre_popularity, get_song_count_by_genre,
+                                count_songs, artist_songs)
 
-client = TestClient(app)
 
-from app.mymodules.info import genre_popularity, get_song_count_by_genre, count_songs, artist_songs
+sys.path.insert(0, os.path.abspath(os.path.join(
+                os.path.dirname(__file__), '..')))
+
 
 spotify_songs = pd.read_csv('/app/app/spotify_songs.csv')
 
 
+client = TestClient(app)
+
 
 def test_genre_popularity():
-    """Test the genre_popularity function to ensure it returns a dictionary with genres
-    as keys and floats as values, with popularity scores between 0 and 100."""
+    """Test the genre_popularity function to ensure it returns a dictionary
+    with genres as keys and floats as values, with popularity scores between
+    0 and 100."""
     result = genre_popularity()
     assert isinstance(result, dict)
     for genre, popularity in result.items():
@@ -26,25 +29,26 @@ def test_genre_popularity():
 
 
 def test_get_song_count_by_genre():
-  """Test the get_song_count_by_genre function to ensure it returns a dictionary with
-    subgenres as keys and integers as values, with the sum of values matching the
-    total number of songs in the dataset."""  
+    """Test the get_song_count_by_genre function to ensure it returns a
+    dictionary with subgenres as keys and integers as values, with the
+    sum of values matching the total number of songs in the dataset."""
     result = get_song_count_by_genre()
     assert isinstance(result, dict)
     assert sum(result.values()) == len(spotify_songs)
 
 
 def test_count_songs():
-    """Test the count_songs function to ensure it returns an integer count of all
-    songs in the dataset."""
+    """Test the count_songs function to ensure it returns an integer
+    count of all songs in the dataset."""
     result = count_songs('path_to_your_csv')
     assert isinstance(result, int)
     assert result == len(spotify_songs)
 
 
 def test_artist_songs():
-    """Test the artist_songs function to ensure it returns a dictionary with artists
-    as keys and integers as values, sorted by artist name."""
+    """Test the artist_songs function to ensure it returns a
+    dictionary with artists as keys and integers as values,
+    sorted by artist name."""
     result = artist_songs()
     assert isinstance(result, dict)
     assert list(result.keys()) == sorted(result.keys())
