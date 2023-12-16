1f05c8b3127644deaafdd0d33d330d5735db7a08
alessandrogardenal <891882@stud.unive.it>
2023-12-16 16:53:39 +0100

    Update code in pep-8 and docstring
    Update code in pep-8 and docstring for Feature 1.

--- backend/app/mymodules/Feature_1_avg_price.py
+++ backend/app/mymodules/Feature_1_avg_price.py
@@ -1,10 +1,19 @@
+"""
+This is the module for Feature 1 of the WePole software.
+
+The calculate_average_price function computes the
+average flight ticket price for a selected flight connection.
+
+"""
 
 import pandas as pd
 from df_integrations import flights
 
+
 def calculate_average_price(data, departure, arrival):
     """
-    Calculate the average price for a given departure-arrival connection, regardless of the date.
+    Calculate the average price for a given departure-arrival connection,
+    regardless of the date.
 
     Parameters:
     - data (pd.DataFrame): DataFrame containing flight data.
@@ -15,7 +24,8 @@ def calculate_average_price(data, departure, arrival):
     - Average price for the specified route.
     """
     # Filter DataFrame based on the departure and arrival airports
-    dep_arr_flights = data[(data['Departure'] == departure) & (data['Arrival'] == arrival)]
+    dep_arr_flights = data[(data['Departure'] == departure) &
+                           (data['Arrival'] == arrival)]
 
     # Check if there are valid entries for the given route
     if len(dep_arr_flights) == 0:
@@ -25,4 +35,3 @@ def calculate_average_price(data, departure, arrival):
     average_price = dep_arr_flights['Price in £'].mean()
 
     return average_price
-
