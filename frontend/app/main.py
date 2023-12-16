e0bcd270746209a4852d627b4661df4e6cdd17e1
aurorazilio <975884@stud.unive.it>
2023-12-16 15:50:54 +0100

    Improvement - Aurora
    Improvement of frontend and html file.

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -20,67 +20,11 @@ BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
 
 
 class AddressesForm(FlaskForm):
-    area_name = SelectField('Area name:', choices=[
-        ('DUOMO', 'DUOMO'),
-        ('MAGENTA - S. VITTORE', 'MAGENTA - S. VITTORE'),
-        ('CENTRALE', 'CENTRALE'),
-        ("CITTA' STUDI", "CITTA' STUDI"),
-        ('BARONA', 'BARONA'),
-        ('GUASTALLA', 'GUASTALLA'),
-        ('GIARDINI PORTA VENEZIA','GIARDINI PORTA VENEZIA'),
-        ('ISOLA','ISOLA'),
-        ('BUENOS AIRES', 'BUENOS AIRES'),
-        ('PORTA ROMANA', 'PORTA ROMANA'),
-        ('VIGENTINA', 'VIGENTINA'),
-        ('TORTONA', 'TORTONA'),
-        ('NAVIGLI', 'NAVIGLI'),
-        ('PAGANO','PAGANO'),
-        ('TRE TORRI', 'TRE TORRI'),
-        ('BOVISA', 'BOVISA'),
-        ('GARIBALDI REPUBBLICA', 'GARIBALDI REPUBBLICA'),
-        ('BICOCCA', 'BICOCCA'),
-        ('TICINESE', 'TICINESE'),
-        ('LODI - CORVETTO', 'LODI - CORVETTO'),
-        ('TIBALDI', 'TIBALDI'),
-        ('UMBRIA - MOLISE', 'UMBRIA - MOLISE'),
-        ('DE ANGELI - MONTE ROSA', 'DE ANGELI - MONTE ROSA'),
-        ('BRERA', 'BRERA'),
-        ('GRECO', 'GRECO'),
-        ('SCALO ROMANA', 'SCALO ROMANA'),
-        ('LORENTEGGIO', 'LORENTEGGIO'),
-        ('GIAMBELLINO', 'GIAMBELLINO'),
-        ('SARPI', 'SARPI'),
-        ('PARCO LAMBRO - CIMIANO', 'PARCO LAMBRO - CIMIANO'),
-        ('MACIACHINI - MAGGIOLINA', 'MACHIACHINI - MAGGIOLINA'),
-        ('LORETO', 'LORETO'),
-        ('PADOVA', 'PADOVA'),
-        ('XXII MARZO', 'XXII MARZO'),
-        ('RIPAMONTI', 'RIPAMONTI'),
-        ('BANDE NERE', 'BANDE NERE'),
-        ('S. SIRO', 'S. SIRO'),
-        ('QUARTO OGGIARO', 'QUARTO OGGIARO'),
-        ('FARINI', 'FARINI'),
-        ("NIGUARDA - CA' GRANDA", "NIGUARDA - CA' GRANDA"),
-        ('ENERMIA', 'ENERMIA'),
-        ('EX OM - MORIVIONE', 'EX OM - MORIVIONE'),
-        ('S. CRISTOFORO', 'S. CRISTOFORO'),
-        ('LAMBRATE', 'LAMBRATE'),
-        ('PARCO FORLANINI - ORTICA', 'PARCO FORLANINI - ORTICA'),
-        ('VIALE MONZA', 'VIALE MONZA'),
-        ('STADERS', 'STADERA'),
-        ('WASHINGTON', 'WASHINGTON'),
-        ('PORTELLO', 'PORTELLO'),
-        ('FORZE ARMATE', 'FORZE ARMATE'),
-        ('GALLARATESE', 'GALLARATESE'),
-        ('AFFORI', 'AFFORI'),
-        ('QT 8', 'QT 8'),
-        ('VILLAPIZZONE', 'VILLAPIZZONE'),
-        ('GHISOLFA', 'GHISOLFA'),
-        ('ORTOMERCATO', 'ORTOMERCATO'),
-        ('DERGANO', 'DERGANO'),
-        ('CORSICA', 'CORSICA')
-
-    ])
+    areas = []
+    response = requests.get('http://backend/areas')
+    if response.status_code == 200:
+        areas = response.json()
+    area_name = SelectField('Area name:', choices=[(area, area) for area in areas])
     submit = SubmitField('Get Area_name from FastAPI Backend')
 
 
@@ -135,6 +79,7 @@ def fetch_date_from_backend():
         return 'Area name not available'
 
 
+
 @app.route('/addresses', methods=['GET', 'POST'])
 def addresses():
     """
@@ -148,16 +93,14 @@ def addresses():
 
     if form.validate_on_submit():
         selected_area = form.area_name.data
-
         fastapi_url = f'{FASTAPI_BACKEND_HOST}/addresses/{selected_area}'
         response = requests.get(fastapi_url)
-
         if response.status_code == 200:
             return render_template('addresses.html', form=form, result=response.text, error_message=error_message)
         else:
             error_message = f'Error: Unable to fetch area_name for {selected_area} from FastAPI Backend'
-
     return render_template('addresses.html', form=form, result=None, error_message=error_message)
+
 
 
 @app.route('/provider', methods=['GET', 'POST'])
