d6871c45ec8bf4725717c56a409ed2c0b9be32be
martaperissinotto <150264067+martaperissinotto@users.noreply.github.com>
2023-12-16 12:54:01 +0100

    Enhancement pep-8
    Enhancement of the code following the pep-8 guildlines and little modification in the backend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,21 +13,27 @@ import pandas as pd
 import csv
 from typing import List
 
-
-
 app = FastAPI()
-
-
-from fastapi.responses import HTMLResponse
-
-
-app = FastAPI()
-
 
 df = pd.read_csv('/app/app/ricarica_colonnine.csv', sep=';')
 
+@app.get('/areas', response_model=List[str])
+def get_areas():
+    areas = df['nome_nil'].unique().tolist()
+    return areas
+
+
 @app.get('/addresses/{name}', response_model=List[str])
 def get_via_by_area(name: str):
+    """
+    Endpoint to get a list of street names based on the provided area name.
+
+    Args:
+        name (str): The name of the area.
+
+    Returns:
+        List[str]: A list of street names in the specified area.
+    """
     name_lower = name.lower()
     filtered_data = df[df['nome_nil'].str.lower() == name_lower]
     if not filtered_data.empty:
@@ -35,10 +41,6 @@ def get_via_by_area(name: str):
         return via_list
     else:
         return []
-
-
-
-
 
 
 @app.get('/')
@@ -51,8 +53,19 @@ def read_root():
     """
     return {"Hello": "World"}
 
+
 @app.get('/module/search/{street_name}')
 def get_charging_stations_provider_given_street_name(street_name):
+    """
+    Endpoint to get the charging station provider for a given street name.
+
+    Args:
+        street_name (str): The name of the street.
+
+    Returns:
+        str: A message indicating the provider for the charging station on the specified street. 
+            If the street is not present in the database, a corresponding message is returned.
+    """
     street_name=street_name.upper()
     charging_station=df[df['nome_via']== street_name]
     if not charging_station.empty:
@@ -64,13 +77,13 @@ def get_charging_stations_provider_given_street_name(street_name):
 @app.get('/module/lookfor/{provider_name}')
 def get_charging_points_by_provider(provider_name):
     """
-    Retrieve charging points based on the provided provider's name.
+    Endpoint to get charging points based on the provided provider's name.
 
     Args:
-    - provider_name (str): The name of the charging point provider.
+        provider_name (str): The name of the charging point provider.
 
     Returns:
-    - List[dict]: List of dictionaries containing charging point information.
+        List[dict]: List of dictionaries containing charging point information.
     """
     charging_points = []
     with open('/app/app/ricarica_colonnine.csv', newline='', encoding='utf-8') as csvfile:
@@ -88,14 +101,26 @@ def get_charging_points_by_provider(provider_name):
 
 @app.get('/get_charging_point')
 def numbers_of_stations_per_via(street_name):
+    """
+    Endpoint to get the number of charging stations based on the provided street name.
+
+    Args:
+        street_name (str): The name of the street.
+
+    Returns:
+        str: A message indicating the number of charging stations in the specified street.
+            If the street is not present in the dataset, a corresponding message is returned.
+    """
+
     selected_street = df[df['nome_via'] == street_name]
     
     if not selected_street.empty:
-        number_of_stations = selected_street['numero_col'].sum()  # Sum the column values for the selected street
+        number_of_stations = selected_street['numero_col'].sum()
         info_vie = number_of_stations
-        return f"Number of charging stations in '{street_name}': {info_vie}"
+        return f"The number of charging stations in '{street_name}' is {info_vie}"
     else:
-        return f"The sreet '{street_name}' is not present in the dataset."
+        return f"The street '{street_name}' is not present in the dataset."
+
 
 @app.get('/get_charging_stations/{street_name}')
 def numbers_of_stations_per_via(street_name: str):
@@ -103,48 +128,68 @@ def numbers_of_stations_per_via(street_name: str):
     Endpoint to get the number of charging columns based on the street name.
 
     Args:
-        nome_via (str): The name of the street.
+        street_name (str): The name of the street.
 
     Returns:
         dict: Information about the number of charging columns for the provided street name.
     """
-    street_name = street_name.upper()  # Convert to title case for consistency
+    street_name = street_name.upper()  
     selected_street = df[df['nome_via'] == street_name]
     
     if not selected_street.empty:
-        number_of_stations = selected_street['numero_col'].sum()  # Sum the column values for the selected street
-        return {"street_name": street_name, "number_stations": str(number_of_stations)}
+        number_of_stations = selected_street['numero_col'].sum()  
+        return f'The number of charging stations in {street_name} is {str(number_of_stations)}'
     else:
         return {"error": f"The street '{street_name}' is not present in the dataset."}
 
+
 def numbers_of_stations_per_via(street_name_input: str = None):
+    """
+    Retrieve the number of charging stations based on the provided street name.
+
+    Args:
+        street_name_input (str, optional): The name of the street. Defaults to None.
+
+    Returns:
+        Union[str, dict]: If street_name_input is provided, returns information about
+                          the number of charging stations for the provided street name.
+                          If street_name_input is None, returns a default message.
+    """
+
     if street_name_input is not None:
         result = numbers_of_stations_per_via(street_name_input)
         return result
     else:
         return '/'
 
+
 def get_socket_types_by_zone(zone: str):
+    """
+    Retrieve the types of charging sockets based on the provided zone.
+
+    Args:
+        zone (str): The name of the zone.
+
+    Returns:
+        str: A message indicating the types of charging sockets in the specified zone.
+            If the zone is not found, an HTTPException with status code 404 is raised.
+            If an unexpected error occurs, an HTTPException with status code 500 is raised.
+    """
     try:
-        # Filter the DataFrame for the specified zone
         zone=zone.upper()
         zone_data = df[df['localita'] == zone]
-
-        # Check if the zone exists
         if zone_data.empty:
             raise HTTPException(status_code=404, detail=f"Zone '{zone}' not found.")
-
-        # Get the unique socket types in the zone
         socket_types = zone_data['infra'].unique().tolist()
 
-        return {"zone": zone, "socket type": socket_types}
+        return f'In {zone} the type of socket is {socket_types}'
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/socket_types_by_zone/{zone}")
 async def socket_types_by_zone(zone: str):
     return get_socket_types_by_zone(zone)
-
 
 
 @app.get('/get-date')
--- frontend/app/main.py
+++ frontend/app/main.py
@@ -7,22 +7,17 @@ This module defines a simple Flask application that serves as the frontend for t
 
 
 from flask import Flask, render_template, request
-import requests  # Import the requests library to make HTTP requests
+import requests
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-# Configuration for the FastAPI backend URL
-FASTAPI_BACKEND_HOST = 'http://backend'  # Replace with the actual URL of your FastAPI backend
+
+FASTAPI_BACKEND_HOST = 'http://backend'
 BACKEND_URL = f'{FASTAPI_BACKEND_HOST}/query/'
 
-
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'  
-
-FASTAPI_BACKEND_HOST = 'http://backend'  
 
 class AddressesForm(FlaskForm):
     area_name = SelectField('Area name:', choices=[
@@ -88,6 +83,7 @@ class AddressesForm(FlaskForm):
     ])
     submit = SubmitField('Get Area_name from FastAPI Backend')
 
+
 class ProviderForm(FlaskForm):
     provider_name = SelectField()
 
@@ -106,13 +102,29 @@ class ZoneForm(FlaskForm):
     zone_name=StringField('Zone name')
     submit=SubmitField('Get types of sockets from FASTAPI Backend')
     
+
 @app.route('/')
 def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: Rendered HTML content for the index page.
+    """
+
     date_from_backend = fetch_date_from_backend()
     return render_template('index.html', date_from_backend=date_from_backend)
   
 
 def fetch_date_from_backend():
+    """
+    Fetch the current date from the backend.
+
+    Returns:
+        str: The current date in ISO format, or a default message if not available.
+    """
+
+
     backend_url = 'http://backend/get-date'
     try:
         response = requests.get(backend_url)
@@ -122,8 +134,15 @@ def fetch_date_from_backend():
         print(f"Error fetching date from backend: {e}")
         return 'Area name not available'
 
+
 @app.route('/addresses', methods=['GET', 'POST'])
 def addresses():
+    """
+    Render the addresses page.
+
+    Returns:
+        str: Rendered HTML content for the addresses page in order to show street names present in a specific zone.
+    """
     form = AddressesForm()
     error_message = None 
 
@@ -143,8 +162,14 @@ def addresses():
 
 @app.route('/provider', methods=['GET', 'POST'])
 def provider():
-    error_message = None  # Initialize error message
-    form = None  # Initialize form as None
+    """
+    Render the provider page.
+
+    Returns:
+        str: Rendered HTML content for the provider page in order to know which street a specific provider is present.
+    """
+    error_message = None  
+    form = None 
 
     if request.method == 'POST':
         provider_name = request.form.get('provider_name')
@@ -153,16 +178,24 @@ def provider():
         
         if response.status_code == 200:
             data = response.json()
-            form = ProviderForm()  # Reinitialize form if needed
+            form = ProviderForm()
             return render_template('provider.html', form=form, result=data, error_message=error_message)
         else:
             error_message = f'Error: Unable to fetch data for {provider_name} from FastAPI Backend'
 
-    form = ProviderForm()  # Initialize form for initial GET request
+    form = ProviderForm()
     return render_template('provider.html', form=form, result=None, error_message=error_message)
+
 
 @app.route('/street_name', methods=['GET','POST'])
 def provider_street_name():
+    """
+    Render the street name page.
+
+    Returns:
+        str: Rendered HTML content for the street name page in order to know which provider is present in the street written as input.
+    """
+
     form = ProviderstreetnameForm()
     error_message= None
 
@@ -178,28 +211,24 @@ def provider_street_name():
     return render_template('street_name1.html', form=form, result=None, error_message=error_message)
 
 
-
-
 @app.route('/number_stations', methods=['GET', 'POST'])
 def number_stations():
     """
-    Render the internal page.
+    Render the number of stations page.
 
     Returns:
-        str: Rendered HTML content for the index page.
+        str: Rendered HTML content for the number of stations page to know how many charging stations are present in a specific street.
     """
+
     form = QueryForm()
-    error_message = None  # Initialize error message
+    error_message = None
 
     if form.validate_on_submit():
         street_name = form.street_name.data
-
-        # Make a GET request to the FastAPI backend
         fastapi_url = f'{FASTAPI_BACKEND_HOST}/get_charging_stations/{street_name}'
         response = requests.get(fastapi_url)
 
         if response.status_code == 200:
-            # Extract and display the result from the FastAPI backend
             data = response.json()
             return render_template('number_stations.html', form=form, result=data, error_message=error_message)
         else:
@@ -210,6 +239,12 @@ def number_stations():
     
 @app.route('/socket_types', methods=['GET', 'POST'])
 def socket_types():
+    """
+    Render the socket types page.
+
+    Returns:
+        str: Rendered HTML content for the socket types page to discover which type of socket (AC Normal or AC-DC Fast) is present in the street written as input.
+    """
     error_message = None
     form = None
 
