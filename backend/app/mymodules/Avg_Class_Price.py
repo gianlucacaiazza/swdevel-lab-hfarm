c1f8f65a740eaa0c6151f969dfb495457acb456b
MarcoScuccato <891694@stud.unive.it>
2023-12-17 16:46:57 +0100

    PEP8 Formatting and Html Docstring

--- backend/app/mymodules/Avg_Class_Price.py
+++ backend/app/mymodules/Avg_Class_Price.py
@@ -1,29 +1,29 @@
 
+'''
+Feature 3 - Average Price Per Flight Class
 
-from df_integrations import flights
-import pandas as pd
+This module provides functionality to calculate the average prices
+for different types of flight classes offered by a specific airline.
+'''
 
 
 def calculate_average_price_airline(flights, airline):
-    """ 
+    """
     Calculate the average price for each travel class of a specific airline.
 
     Parameters:
-    - flights (DataFrame): A DataFrame containing flight data, including columns 'Air Carrier',
+    - flights (DataFrame): A DataFrame containing flight data,
+                          including columns 'Air Carrier',
                           'Travel Class', and 'Price in £'.
-    - airline (str): The name of the airline for which to calculate average prices.
+    - airline (str): The name of the airline for which to calculate avg prices.
 
     Returns:
-    str: A string containing information about the average prices for each travel class of the specified airline.
-         If the airline has only one travel class, additional information about the class is included.
-         If the airline has no flights recorded in the DataFrame, appropriate information is provided.
-
-    Example Usage:
-    result = calculate_average_price_airline(flights, 'YourAirlineName')
-    print(result)
-    Average Price ECONOMY: 123.45 £
-    Average Price BUSINESS: 789.60 £
-    The airline only has ECONOMY class flights
+    str: A string containing information about the average prices for each
+         travel class of the specified airline.
+         If the airline has only one travel class, additional information
+         about the class is included.
+         If the airline has no flights recorded in the DataFrame, appropriate
+         information is provided.
     """
     # Filter the DataFrame for the specified airline
     airline_data = flights[flights['Air Carrier'] == airline]
@@ -32,7 +32,7 @@ def calculate_average_price_airline(flights, airline):
     travel_classes = airline_data['Travel Class'].unique()
 
     # Initialize a string to store the results
-    results_string = f''
+    res_str = f''
 
     # Calculate the average price for each travel class
     for travel_class in travel_classes:
@@ -40,12 +40,11 @@ def calculate_average_price_airline(flights, airline):
         class_data = airline_data[airline_data['Travel Class'] == travel_class]
         # Calculate the average price for the travel class
         average_price = class_data['Price in £'].mean()
-        results_string += f'Average Price {travel_class}: {average_price:.2f} £'
+        res_str += f'Average Price {travel_class}: {average_price:.2f} £'
 
     # Check if there is only one travel class
     if len(travel_classes) == 1:
-        results_string += f" The airline only has {travel_classes[0]} class flights"
-
+        res_str += f" The airline only has {travel_classes[0]} class flights"
 
     # Return the results as a string
-    return results_string
+    return res_str
